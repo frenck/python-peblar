ebbaa4e2c6a0741425a5948d7d7c0a1f3af69780
Franck Nijhof <git@frenck.dev>
2024-12-18 16:11:16 +0000

    Export all models in package root

--- src/peblar/__init__.py
+++ src/peblar/__init__.py
@@ -1,5 +1,14 @@
 """Asynchronous Python client for Peblar EV chargers."""
 
+from .const import (
+    AccessMode,
+    ChargeLimiter,
+    CPState,
+    LedIntensityMode,
+    SmartChargingMode,
+    SolarChargingMode,
+    SoundVolume,
+)
 from .exceptions import (
     PeblarAuthenticationError,
     PeblarConnectionError,
@@ -7,13 +16,37 @@ from .exceptions import (
     PeblarError,
     PeblarResponseError,
 )
-from .peblar import Peblar
+from .models import (
+    PeblarEVInterface,
+    PeblarHealth,
+    PeblarMeter,
+    PeblarSystem,
+    PeblarSystemInformation,
+    PeblarUserConfiguration,
+    PeblarVersions,
+)
+from .peblar import Peblar, PeblarApi
 
 __all__ = [
+    "AccessMode",
+    "CPState",
+    "ChargeLimiter",
+    "LedIntensityMode",
     "Peblar",
+    "PeblarApi",
     "PeblarAuthenticationError",
     "PeblarConnectionError",
     "PeblarConnectionTimeoutError",
+    "PeblarEVInterface",
     "PeblarError",
+    "PeblarHealth",
+    "PeblarMeter",
     "PeblarResponseError",
+    "PeblarSystem",
+    "PeblarSystemInformation",
+    "PeblarUserConfiguration",
+    "PeblarVersions",
+    "SmartChargingMode",
+    "SolarChargingMode",
+    "SoundVolume",
 ]
